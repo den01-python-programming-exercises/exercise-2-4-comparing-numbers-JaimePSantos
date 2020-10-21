af8e265fa9399901dcf60efaeaf6567be53ca499
Jaime Santos <50669048+JaimePSantos@users.noreply.github.com>
2020-10-21 12:43:24 +0100

    ex24

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #write your code below this line
+    number1 = int(input())
+    number2 = int(input())
+    if(number1>number2):
+        print(f"{number1} is greater than {number2}.")
+    elif(number2>number1):
+        print(f"{number1} is smaller than {number2}.")
+    else:
+        print(f"{number1} is equal to {number2}.")
 
 if __name__ == '__main__':
     main()
